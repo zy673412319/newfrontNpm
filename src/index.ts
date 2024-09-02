c3bd87bc483250e4c8ae8df79c6f7e302ef68a05
zhangyu <673412319@qq.com>
2024-09-02 16:46:04 +0800

    处理兼容js

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
 // import MyComponent from './MyComponent.vue';
 
-import { routineChecker,
-  carNoQChecker,
-  idCardChecker,
-  bankCardChecker,
-  cnChecker,
-  numChecker,
-  passportChecker } from './utils/regularCheck';
+import { routineChecker, carNoQChecker, idCardChecker, bankCardChecker, cnChecker, numChecker, passportChecker } from './utils/regularCheck';
 import {
   telFormat, firstLetterUpper, dateFormat, beforeDateFormat, 
   calculateDiffTime, getWeek, numFilterThreeCut, digitUppercase,
-  changeToChinese, urlToObject,
-  uniqueArray,
-  uniqueArrayByProperty,
-  uniqueArrayAndObject,
-  sortArray,
-  getLastTime,
-  getDateOfWeek
+  changeToChinese, urlToObject, uniqueArray, uniqueArrayByProperty,
+  uniqueArrayAndObject, sortArray, getLastTime, getDateOfWeek
 } from './utils/commonMethod'
 
 export { 
@@ -45,3 +34,13 @@ export {
   getLastTime,
   getDateOfWeek
 };
+
+// import { VueConstructor } from 'vue';
+// import { PluginFunction } from 'vue';
+// import Modal from './pages/MyModal.vue';
+
+// export default {
+//   install(Vue: VueConstructor): void {
+//     Vue.component('Modal', Modal);
+//   },
+// };
